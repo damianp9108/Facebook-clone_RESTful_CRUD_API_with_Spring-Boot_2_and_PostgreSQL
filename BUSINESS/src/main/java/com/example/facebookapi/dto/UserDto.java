b519bff688fd00af3dd954f72d88920bd3d0d45b
Damian Pyda <damianp91@gmail.com>
2021-11-20 19:05:24 +0100

    relacja oneToMany, ManyToOne miedzy User a Post, JsonIgnoreProperties, naprawa bledow odpowiedzi dla uzytkownika

--- BUSINESS/src/main/java/com/example/facebookapi/dto/UserDto.java
+++ BUSINESS/src/main/java/com/example/facebookapi/dto/UserDto.java
@@ -1,16 +1,23 @@
 package com.example.facebookapi.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
+import java.util.List;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserDto {
 
+    @NotNull(message = "Prosze podac nazwe uzytkownika")
     @Pattern(regexp = "^((?!.*[\\s]).{4,14})", message = "nazwa uzytkownia musi zawierac 4-14 znakow, spacja niedozwolona")
     private String userName;
     private String userImage;
+
+    @JsonIgnoreProperties("userDto")
+    private List<PostDto> postsDTO;
 }
