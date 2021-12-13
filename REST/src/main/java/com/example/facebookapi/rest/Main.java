885a033f871b4391458161a0394477ef4b505878
Damian Pyda <damianp91@gmail.com>
2021-12-13 08:18:53 +0100

    modyfikacja adnotacji spring-bootowych  klasy Main

--- REST/src/main/java/com/example/facebookapi/rest/Main.java
+++ REST/src/main/java/com/example/facebookapi/rest/Main.java
@@ -3,12 +3,14 @@ package com.example.facebookapi.rest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-
-@SpringBootApplication(scanBasePackages = {"com.example.facebookapi.business", "com.example.facebookapi.domain", "com.example.facebookapi.rest"})
-@EntityScan(basePackages = "com.example.facebookapi.domain.entity")
-@EnableJpaRepositories(basePackages = "com.example.facebookapi.domain.repository")
+// {"com.example.facebookapi.business", "com.example.facebookapi.domain", "com.example.facebookapi.rest"}
+@SpringBootApplication
+@ComponentScan(basePackages = "com.example.facebookapi")
+@EntityScan(basePackages = "com.example.facebookapi")
+@EnableJpaRepositories(basePackages = "com.example.facebookapi")
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
