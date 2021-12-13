a2bf2326dc88e59d31fb1cc838803994cba82f55
Damian Pyda <damianp91@gmail.com>
2021-12-13 08:48:12 +0100

    utworzenie modulu 'application'

--- application/src/main/java/com/example/facebookapi/application/Main.java
+++ application/src/main/java/com/example/facebookapi/application/Main.java
@@ -1,4 +1,4 @@
-package com.example.facebookapi.rest;
+package com.example.facebookapi.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-// {"com.example.facebookapi.business", "com.example.facebookapi.domain", "com.example.facebookapi.rest"}
 @SpringBootApplication
 @ComponentScan(basePackages = "com.example.facebookapi")
 @EntityScan(basePackages = "com.example.facebookapi")
